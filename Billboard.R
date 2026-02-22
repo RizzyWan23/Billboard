930581c1b01fa9a3e964f7e3080c65a3a638d40d
Redwanul <redwanul.hoq23@gmail.com>
2026-02-22 13:25:08 +0600

    Initial commit

---
+++ Billboard.R
@@ -0,0 +1,11 @@
+print("This file was created within RStudio")
+
+print("And now it will be on GitHub")
+
+install.packages("usethis")
+library(usethis)
+usethis::create_github_token()
+
+
+usethis::edit_r_environ()
+usethis::git_sitrep()
